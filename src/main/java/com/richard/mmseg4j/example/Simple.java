cb1c1286236bf4d8fe6e77286eedc2577c9e2e69
richard <liuyngchng@hotmail.com>
2021-06-24 08:33:52 +0800

    init

---
+++ src/main/java/com/richard/mmseg4j/example/Simple.java
@@ -0,0 +1,23 @@
+package com.richard.mmseg4j.example;
+
+import java.io.IOException;
+
+import com.richard.mmseg4j.Seg;
+import com.richard.mmseg4j.SimpleSeg;
+
+/**
+ * 
+ * @author chenlb 2009-3-14 上午12:38:40
+ */
+public class Simple extends Complex {
+	
+	protected Seg getSeg() {
+
+		return new SimpleSeg(dic);
+	}
+
+	public static void main(String[] args) throws IOException {
+		new Simple().run(args);
+	}
+
+}
